98ac93c99b9dfc6b6493e9d2f8934f3df0cb7de2
aunikahmed <aunikahmed80@gamil.com>
2015-10-10 23:22:00 +0600

    first upload

---
+++ db31.sql
@@ -0,0 +1,10 @@
+select * from idf_diagnosis;
+desc idf_diagnosis;
+select count(*) from idf_diagnosis;
+
+select  client_id ,count(client_id)  from idf_diagnosis group by client_id order by client_id;
+
+select count(distinct client_id) from idf_diagnosis;
+
+select count(*) from client;
+
